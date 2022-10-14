2cebf036b96b29b99ddf9138b0c0e6416cd81259
Jasson Luiz <jassonluizjr@hotmail.com>
2022-10-14 13:48:16 -0300

    First commit

---
+++ src/main/java/io/github/jassonluiz/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package io.github.jassonluiz.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jassonluiz/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jassonluiz.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
